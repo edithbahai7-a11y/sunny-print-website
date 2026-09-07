a9bd9582190824de76f735a93c1c4ee577560e48
Edith Bahai <YOUR-GITHUB-EMAIL>
2026-09-07 15:45:30 +0300

    Fix responsive mobile layout

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,36 +1,48 @@
-
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <nav className="navbar">
-
       <div className="navbar-container">
 
         {/* LOGO */}
-        <Link to="/" className="navbar-logo">
+        <Link to="/" className="navbar-logo" onClick={closeMenu}>
           <img
             src="/images/sunny-print-logo.png"
             alt="Sunny Print"
           />
         </Link>
 
-        {/* NAVIGATION */}
-        <div className="navbar-links">
-
-          <Link to="/">Home</Link>
-
-          <Link to="/about">About Us</Link>
-
-          <Link to="/services">Products & Services</Link>
-
-          <Link to="/contact">Contact</Link>
-
+        {/* DESKTOP NAVIGATION */}
+        <div className={`navbar-links ${menuOpen ? "mobile-open" : ""}`}>
+          <Link to="/" onClick={closeMenu}>Home</Link>
+          <Link to="/about" onClick={closeMenu}>About Us</Link>
+          <Link to="/services" onClick={closeMenu}>
+            Products & Services
+          </Link>
+          <Link to="/contact" onClick={closeMenu}>Contact</Link>
         </div>
 
-      </div>
+        {/* MOBILE MENU BUTTON */}
+        <button
+          className="menu-button"
+          onClick={() => setMenuOpen(!menuOpen)}
+          aria-label="Toggle navigation menu"
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
 
+      </div>
     </nav>
   );
 }
